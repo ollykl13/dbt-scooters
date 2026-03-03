6266cf2a464d45dc8cfaf1e2f194d3147bbbeed5
ollykl13 <ollykl@yandex.ru>
2026-03-03 16:55:06 +0300

    Add incremental events_clean

---
+++ models/events_clean.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+	user_id 
+	,"timestamp" 
+	,type_id
+FROM {{ source("scooters_raw", "events") }}
+{% if is_incremental() %}
+WHERE
+    "timestamp" > (SELECT max("timestamp") FROM {{ this }})
+{% else %}
+WHERE
+    "timestamp" < '2023-08-01'::date
+{% endif %}
