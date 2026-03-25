d96b85edc4a671c8d08c9e1541498712ae607771
ollykl13 <ollykl@yandex.ru>
2026-03-25 22:19:12 +0300

    Добавление внешнего параметра

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,13 +1,17 @@
+{% set date = var("date", none) %}
 SELECT DISTINCT
 	user_id 
 	,"timestamp" 
 	,type_id
 	,{{ updated_at() }}
 FROM {{ source("scooters_raw", "events") }}
+WHERE
 {% if is_incremental() %}
-WHERE
-    "timestamp" > (SELECT max("timestamp") FROM {{ this }})
+    {% if date %}
+        date("timestamp") = date '{{ date }}'
+    {% else %}
+    	"timestamp" > (SELECT max("timestamp") FROM {{ this }})
+    {% endif %}
 {% else %}
-WHERE
-    "timestamp" < '2023-08-01'::date
+    "timestamp" < timestamp '2023-08-01'
 {% endif %}
