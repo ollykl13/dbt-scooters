beb59307f35292a50d6397c20587e30697e9d16d
ollykl13 <ollykl@yandex.ru>
2026-04-22 22:00:00 +0300

    Добавление макросов для метаданных

--- models/events_full.sql
+++ models/events_full.sql
@@ -1,6 +1,7 @@
 SELECT 
 	ec.*
 	,et.type
+	,date(ec."timestamp") as "date"
 FROM {{ ref("events_clean") }} AS ec
 	LEFT JOIN {{ ref("event_types") }} AS et 
-		ON et.type_id = ec.type_id
+		ON et.type_id = ec.type_id	
